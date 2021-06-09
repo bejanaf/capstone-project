9a1b4966274df170bb723e2de7b46693e1ce18ef
bejan afshar <cosmopolit_b@hotmail.de>
2021-06-09 16:23:13 +0200

    create GlobalStyles.js and importet to design position of navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,32 @@
+import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
+import NewsIcon from '../image/news-icon.svg';
+import BitcoinIcon from '../image/btc-icon.svg';
+import WalletIcon from '../image/wallet.svg';
 
 export default function Navigation() {
   return (
-    <div>
+    <NavigationWrapper>
       <NavLink exact to="/" className="navButton">
-        News
+        <NavigationIcons src={NewsIcon} alt="Icon for News" />
       </NavLink>
 
       <NavLink to="/bookmarks" className="navButton">
-        Bookmarks
+        <NavigationIcons src={BitcoinIcon} alt="Icon of a Bitcoin" />
       </NavLink>
 
       <NavLink to="/wallet" className="navButton">
-        Wallet
+        <NavigationIcons src={WalletIcon} alt="Icon of a Wallet" />
       </NavLink>
-    </div>
+    </NavigationWrapper>
   );
 }
+
+const NavigationIcons = styled.img`
+  width: 2rem;
+`;
+
+const NavigationWrapper = styled.div`
+  border: 3px solid black;
+  width: 90vw;
+`;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GlobalStyle from './GloabalStyle';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <GlobalStyle />
       <App />
     </Router>
   </React.StrictMode>,
