42c421f7d582427248580391a3be8f5f43483964
bejan afshar <cosmopolit_b@hotmail.de>
2021-06-09 16:58:28 +0200

    styled the position of the navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,26 +7,37 @@ import WalletIcon from '../image/wallet.svg';
 export default function Navigation() {
   return (
     <NavigationWrapper>
-      <NavLink exact to="/" className="navButton">
-        <NavigationIcons src={NewsIcon} alt="Icon for News" />
-      </NavLink>
+      <NavBar>
+        <NavLink exact to="/" className="navButton">
+          <NavigationIcons src={NewsIcon} alt="Icon for News" />
+        </NavLink>
 
-      <NavLink to="/bookmarks" className="navButton">
-        <NavigationIcons src={BitcoinIcon} alt="Icon of a Bitcoin" />
-      </NavLink>
+        <NavLink to="/bookmarks" className="navButton">
+          <NavigationIcons src={BitcoinIcon} alt="Icon of a Bitcoin" />
+        </NavLink>
 
-      <NavLink to="/wallet" className="navButton">
-        <NavigationIcons src={WalletIcon} alt="Icon of a Wallet" />
-      </NavLink>
+        <NavLink to="/wallet" className="navButton">
+          <NavigationIcons src={WalletIcon} alt="Icon of a Wallet" />
+        </NavLink>
+      </NavBar>
     </NavigationWrapper>
   );
 }
 
 const NavigationIcons = styled.img`
+  margin: 0.3rem 0;
   width: 2rem;
 `;
 
 const NavigationWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const NavBar = styled.div`
   border: 3px solid black;
+  border-radius: 100vh;
+  display: flex;
+  justify-content: space-around;
   width: 90vw;
 `;
