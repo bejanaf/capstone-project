9e95ab37af03d8c3040069a88b4886968882a5fc
bejan afshar <cosmopolit_b@hotmail.de>
2021-06-13 13:38:04 +0200

    code a node fetch from api

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 import express from 'express';
 import cors from 'cors';
+import fetch from 'node-fetch';
 
 const server = express();
+
+const apiKey = process.env.API_KEY;
+
+fetch(
+  `https://newsapi.org/v2/everything?q=bitcoin&language=en&sortBy=publishedAt&apiKey=${apiKey}`
+)
+  .then((results) => results, json())
+  .then((data) => data);
 
 server.use(cors());
 
@@ -12,7 +21,9 @@ server.listen(4000);
 // import fetch from 'node-fetch'
 
 // const apiKey = process.env.API_KEY;
-// https://newsapi.org/v2/everything?q=bitcoin&from=2021-05-11&sortBy=publishedAt&apiKey=910c6caaee6945439c19ae099321f342
-// fetch(apiAddress + apiKey).then().then()
+
+// https://newsapi.org/v2/everything?q=bitcoin&from=2021-05-11&sortBy=publishedAt&apiKey=apiKey
+
+// https://newsapi.org/v2/everything?q=bitcoin&from=2021-05-11&sortBy=publishedAt&apiKey + apiKey).then().then()
 
 // http://localhost:4000/news
