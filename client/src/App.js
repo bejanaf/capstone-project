653b88fe27beaba3bdfdc8f0b51d9be8da1dbd81
bejan afshar <cosmopolit_b@hotmail.de>
2021-06-22 20:43:37 +0200

    bug fixed in the route

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import { Switch, Route } from 'react-router-dom';
+import WelcomePage from './pages/WelcomePage';
 import News from './pages/News';
 import Bookmarks from './pages/Bookmarks';
 import Wallet from './pages/Wallet';
-import Navigation from './components/Navigation';
 import { useEffect, useState } from 'react';
 
 function App() {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:4000/news')
+    fetch('http://localhost:4000/api/news')
       .then((res) => res.json())
       .then((response) => {
         console.log(response);
@@ -21,16 +21,21 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/">
+          <WelcomePage />
+        </Route>
+
+        <Route path="/news">
           <News articles={articles} />
         </Route>
+
         <Route path="/bookmarks">
           <Bookmarks />
         </Route>
+
         <Route path="/wallet">
           <Wallet />
         </Route>
       </Switch>
-      <Navigation />
     </div>
   );
 }
