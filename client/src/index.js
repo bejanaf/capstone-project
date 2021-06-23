5192969a0e6bb127425a127806fcd312d1970b28
bejan afshar <cosmopolit_b@hotmail.de>
2021-06-23 12:58:09 +0200

    renamed the GlobalStyle.js source

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import GlobalStyle from './GloabalStyle';
+import GlobalStyle from './GlobalStyle';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
